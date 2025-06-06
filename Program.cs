72479484d3d0976c6a44e3390950e4772dc00128
Miguel Dias <mdias1300@gmail.com>
2025-06-06 19:44:25 -0300

    atividade feita

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿Console.Clear();
+
+Console.WriteLine("Bem-vindo ao comparador de etanol e gasolina:");
+Console.Write("Digite o preço do litro da gasolina: ");
+
+decimal precoGasolina = Convert.ToDecimal(Console.ReadLine());
+
+Console.Write("Digite o preço do litro do etanol: ");
+decimal precoEtanol = Convert.ToDecimal(Console.ReadLine());
+
+decimal relacao = precoEtanol / precoGasolina;
+
+ decimal razao = calcularRazao(precoEtanol, precoGasolina);
+    decimal percentual = razao * 100;
+
+Console.WriteLine($"\nOpreço do etanol corresponde a {percentual:F1}% do preço da gasolina.");
+
+if (ValeAPenaGasolina(relacao))
+    Console.WriteLine("\nRecomendação: Abasteça com Gasolina!");
+else
+    Console.WriteLine("\nRecomendação: Abasteça com Etanol!");
+
+decimal calcularRazao(decimal etanol, decimal gasolina)
+{
+    return etanol / gasolina;
+}
+
+bool ValeAPenaGasolina(decimal relacao)
+{
+    return relacao > 0.73m;
+}
